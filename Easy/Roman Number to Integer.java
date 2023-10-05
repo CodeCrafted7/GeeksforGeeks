ad8c1886fb2d4bdb927d243979d712e44c645421
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2023-10-05 07:20:10 +0530

    Create Roman Number to Integer.java

---
+++ Easy/Roman Number to Integer.java
@@ -0,0 +1,25 @@
+class Solution {
+    // Finds decimal value of a given roman numeral
+  public int romanToDecimal(String str) {
+        int sum = 0, prev = 0;
+        for(int i= str.length()-1; i>=0; i--){
+            int num = getVal(str.charAt(i));
+            sum += (prev > num) ? -num : num;
+            prev = num;
+        }
+        return sum;
+    }
+
+    private static int getVal(char c) {
+        switch(c) {
+			case 'I':return 1;
+			case 'V':return 5;
+			case 'X':return 10;
+			case 'L':return 50;
+			case 'C':return 100;
+			case 'D':return 500;
+			case 'M':return 1000;
+			default:return 0;
+        }
+    }
+}
