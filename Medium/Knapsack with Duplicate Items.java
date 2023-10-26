8e28f5decdec29eba184bd6ab9cfb95c1ccf5bd5
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2023-10-26 17:09:30 +0530

    Create Knapsack with Duplicate Items.java

---
+++ Medium/Knapsack with Duplicate Items.java
@@ -0,0 +1,27 @@
+
+class Solution{
+    static int knapSack(int N, int W, int val[], int wt[])
+    {
+        // code here
+        int[] dp = new int[W+1];
+        dp[0] = 0;
+        int max = 0;
+        int tbagc = 0;
+        for(int bagc = 1;bagc<=W;bagc++) {
+            max = 0;
+            for(int j = 0;j<N;j++) {
+                if(wt[j] <= bagc) {
+                    int rbagc = bagc - wt[j];
+                    tbagc = val[j] + dp[rbagc];
+                }
+                if(max < tbagc) {
+                    max = tbagc;
+                }
+            }
+            dp[bagc] = max;
+        }
+        return dp[W];         
+
+    }
+}  
+
