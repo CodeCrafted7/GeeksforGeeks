9a53699c378d4275ee8182234a9a7d493ca56615
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2023-11-23 16:18:16 +0530

    Create Symmetric Tree.java

---
+++ Easy/Symmetric Tree.java
@@ -0,0 +1,27 @@
+class GfG
+{
+    
+    private static boolean mirror(Node l, Node r) {
+        if(l==null ^ r==null) {
+            return false;
+        } else {
+            if(l==null) {
+                return true;
+            } else {
+                if(l.data == r.data) {
+                    return mirror(l.left, r.right) && mirror(l.right, r.left);
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+ 
+    public static boolean isSymmetric(Node root)
+    {
+        if(root==null) {
+            return true;
+        }
+        return mirror(root.left, root.right);
+    }
+}
