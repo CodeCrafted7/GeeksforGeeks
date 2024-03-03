93031452a17cb6d430defb78c45161b1677758c5
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2024-03-03 09:53:21 +0530

    Create _First element to occur k times.java

---
+++ Easy/_First element to occur k times.java
@@ -0,0 +1,18 @@
+class Solution
+{
+    public int firstElementKTime(int n, int k, int[] a) { 
+        //Base case
+        if(k == 0)return -1;
+        if(a.length == 0)return -1;
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        return Arrays.stream(a)
+                     .filter(num ->{ 
+                          map.put(num, map.getOrDefault(num,0)+1);
+                          return map.get(num) == k;
+                          })
+                          .findFirst()
+                          .orElse(-1);
+    } 
+}
