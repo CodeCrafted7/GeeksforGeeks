541815aea25f89c62c04526358a7410dcc242b3a
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2024-03-25 08:50:49 +0530

    Create Insert an Element at the Bottom of a Stack.java

---
+++ Easy/Insert an Element at the Bottom of a Stack.java
@@ -0,0 +1,8 @@
+//User function Template for Java
+class Solution {
+    public Stack<Integer> insertAtBottom(Stack<Integer> st, int x) {
+         st.add(0,x);
+        
+        return st;
+    }
+}
