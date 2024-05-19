e0bc505125b7e2e32190d79f7e1313950edc4a1c
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2024-05-19 09:08:13 +0530

    Create Find the Highest number.java

---
+++ Easy/Find the Highest number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findPeakElement(List<Integer> a) {
+        int i=0,r=a.size()-1;
+        while(i<r){
+            int m=i+(r-i)/2;
+            if(a.get(m)>a.get(m+1))r=m;
+            else i=m+1;
+        }
+        return a.get(i);
+    }
+}
