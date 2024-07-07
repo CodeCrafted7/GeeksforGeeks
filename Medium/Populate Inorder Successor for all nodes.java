35d1d11dbc32aba845136145a77cb442cb5d24cc
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2024-07-07 09:43:03 +0530

    Create Populate Inorder Successor for all nodes.java

---
+++ Medium/Populate Inorder Successor for all nodes.java
@@ -0,0 +1,26 @@
+
+// User function Template for Java
+
+/*
+class Node {
+    int data;
+    Node left, right,next;
+
+    public Node(int data){
+        this.data = data;
+    }
+}
+*/
+class Solution {
+    Node nxt = null;
+    public void populateNext(Node root) {
+        
+        if(root==null)
+            return;
+            
+        populateNext(root.right);
+        root.next=nxt;
+        nxt=root;
+        populateNext(root.left);
+    }
+}
