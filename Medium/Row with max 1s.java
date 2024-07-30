abe476f1882f24bec76901c5b17107aaf32910cf
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2024-07-30 13:28:20 +0530

    Create Row with max 1s.java

---
+++ Medium/Row with max 1s.java
@@ -0,0 +1,16 @@
+
+class Solution {
+    public int rowWithMax1s(int arr[][]) {
+        int i = 0,j = arr[0].length-1,ans=-1; 
+        while(i<arr.length && j>=0){
+            if(arr[i][j]==0)
+                i++;
+            else
+            {
+                ans=i;
+                j--;
+            }
+        }
+        return ans;
+    }
+}
