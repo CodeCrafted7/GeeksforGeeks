417d7dfda1cd46d7d1cd51fb672d8434b9caa553
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2024-08-07 18:20:01 +0530

    Create Validate an IP Address.java

---
+++ Medium/Validate an IP Address.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValid(String str) {
+        int i=0;int n=str.length();int dotcount=0;
+        if(str.charAt(0)=='.')return false;
+        
+        while(i<n){
+            String temp="";
+            while(i<n && str.charAt(i)>='0' && str.charAt(i)<='9'){
+                // digit=digit*10+str.charAt(i)-'0';
+                temp+=str.charAt(i);
+                i++;
+            }
+            
+            if(temp.length()>1 && temp.charAt(0)=='0')return false; // case for leading 0's like 01.01.01.01
+            int digit=Integer.parseInt(temp);  
+            if(digit>255)return false;
+            
+            if(i<n && i+1<n){
+                if(str.charAt(i)=='.' && str.charAt(i+1)=='.'){ // case for consecutive dots eg: 234...94
+                    return false;
+                }else if(str.charAt(i)=='.'){
+                    dotcount++;
+                }
+            }
+            i++;
+        }
+        
+        return dotcount==3?true:false;
+    }
+}
