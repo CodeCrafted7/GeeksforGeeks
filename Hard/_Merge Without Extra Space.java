7f4accfe240affbb02e8968989a543554ba49194
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2024-12-12 13:38:11 +0530

    Create _Merge Without Extra Space.java

---
+++ Hard/_Merge Without Extra Space.java
@@ -0,0 +1,18 @@
+class Solution {
+    
+    public void mergeArrays(int a[], int b[]) {
+        int i = a.length-1, j = 0;
+        while(i>=0 && j<b.length){
+            if(a[i] < b[j]) break;
+            else{
+                int temp = a[i];
+                a[i] = b[j];
+                b[j] = temp;
+                i--;
+                j++;
+            }
+        }
+        Arrays.sort(a);
+        Arrays.sort(b);
+    }
+}
