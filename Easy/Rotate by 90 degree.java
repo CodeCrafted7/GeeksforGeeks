a7ab85585f1a1eeb266e3f63f8b1cf552f6c2879
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2024-12-22 08:46:36 +0530

    Create Rotate by 90 degree.java

---
+++ Easy/Rotate by 90 degree.java
@@ -0,0 +1,25 @@
+
+class Solution {
+    
+    static void rotateby90(int mat[][]) {
+        int n = mat.length; 
+    
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+
+        
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < n; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[n - i - 1][j];
+                mat[n - i - 1][j] = temp;
+            }
+        }
+    }
+}
+
