9aed35b27d52b7a97c07fb54102d34c7b798e016
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2025-02-04 11:32:52 +0530

    Create .Height of Binary Tree.java

---
+++ Easy/.Height of Binary Tree.java
@@ -0,0 +1,11 @@
+class Solution {
+   
+    int height(Node node) {
+       if(node == null)
+        return -1;
+        
+        int l = height(node.left);
+        int r = height(node.right);
+        return Math.max(l,r)+1;
+    }
+}
